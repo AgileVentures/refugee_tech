52163fa0eb4637dd8da9314c3435fef63ead0899
Zmago Devetak <zmago_devetak@yahoo.com>
2016-03-02 23:05:29 +0000

    initalize api endpoint authentication with devise token auth gem

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,7 @@
 class ApiController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
-
+  
   before_action :destroy_session
   before_action :default_response_format
 
