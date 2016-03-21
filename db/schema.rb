80f14790e8efd9d177cb33b292676a23f2101878
fighterii <fighterii.kb@googlemail.com>
2016-03-21 13:32:43 +0000

    user introduction integrated and tested

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316113545) do
+ActiveRecord::Schema.define(version: 20160321132246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160316113545) do
     t.string   "country"
     t.string   "gender"
     t.inet     "ip_address"
+    t.string   "introduction"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
